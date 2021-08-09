d1b21cc36c339fc1f469d5db7a482198d9ae191e
Gaston Larriera <gaston.larriera@tiarg.com.ar>
2021-08-08 22:10:25 -0300

    resume sections

--- src/modules/professionalInformation/index.js
+++ src/modules/professionalInformation/index.js
@@ -1,15 +1,38 @@
-import React from 'react'
-import { Result, Button } from 'antd'
-import { navigate } from '@reach/router'
+import React, { useState, useEffect, useContext } from 'react'
+import { Card, Row, Col, Typography, Button, Form, Input, DatePicker, Select, InputNumber, Divider } from 'antd';
+import { UserContext } from '../../contexts/userContext';
+import ResumeSection from '../../components/ResumeSection';
+import { resumeSectionsEnum } from '../../constants';
+
+const { Text, Title } = Typography;
 
 export default function ProfessionalInformation() {
+    const [data, setData] = useState({})
+    const [isEditing, setIsEditing] = useState(false)
+    const [isSubmitting, setIsSubmitting] = useState(false)
+
+    const { userData, setUserData } = useContext(UserContext)
+
+    useEffect(() => {
+        setData(userData)
+    }, [userData])
+
+    const getSectionData = (sectionType) => {
+        const sectionData = data?.professionalInformation?.filter(x => x.type == sectionType)
+        return { sectionType: sectionType, sectionData: sectionData }
+    }
+
     return (
         <React.Fragment>
-            <Result
-                status="warning"
-                title="Sección Mi CV en desarrollo"
-                extra={<Button type="primary" key="backToHome" onClick={() => navigate('/dashboard')}>Volver al inicio</Button>}
-            />
+            <Row justify='center'>
+                <Title level={3}>Mi CV</Title>
+            </Row>
+            <Row justify='center'>
+                <Text type='secondary'>Información sobre tu formacion académica y profesional</Text>
+            </Row>
+            <Row justify='center'>
+                {resumeSectionsEnum.map(x => <ResumeSection data={getSectionData(x)} />)}
+            </Row>
         </React.Fragment>
     )
 }
